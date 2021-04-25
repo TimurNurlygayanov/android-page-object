2285dd823d19dee707b40b8c4ff46176a9512c49
Timur Nurlygayanov <v.timur.nurlygayanov@reddit.com>
2021-04-25 15:05:07 +0300

    Added way to work with several elements with the same ids

--- base/elements.py
+++ base/elements.py
@@ -10,25 +10,31 @@ class Element():
     def __init__(self, locator_type, locator_str):
         self._locator = (locator_type, locator_str)
 
-    def find(self, timeout=10):
-        element = None
+    def find(self, many=False):
+        result = None
 
         try:
             if self._locator[0] == 'id':
-                # To make elements locators smaller:
                 locator = self._locator[1]
+
+                # To make elements locators smaller:
                 if ':' not in self._locator[1]:
                     app_name = self._driver.desired_capabilities['appPackage']
                     locator = '{0}:id/' + self._locator[1]
                     locator = locator.format(app_name)
 
-                element = self._driver.find_element_by_id(locator)
+                if many_elements:
+                    result = self._driver.find_elements_by_id(locator)
+                else:
+                    result = self._driver.find_element_by_id(locator)
 
             else:
-                element = self._driver.find_element_by_xpath(self._locator[1])
+                result = self._driver.find_element_by_xpath(self._locator[1])
         except Exception as e:
             print('Can not find element', self._locator)
             print(e)
+
+        return result
 
     def clear(self):
         pass
